c8f69d3a3891db6c51f02a4cd025aab03ebcef0f
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-06-28 19:43:23 +0545

    Updated dat 13

---
+++ python basic/Day13/main.py
@@ -0,0 +1,21 @@
+# class student:
+#     pass
+
+
+# student1=student()
+# student1.name="Dipesh"
+# student1.age=23
+
+# print(student1.name)
+
+
+class student:
+    pass
+
+student1 = student()
+
+student1.name = "Dipesh"
+student1.age = 23
+
+print(student1.name)
+print(student1.age) 
