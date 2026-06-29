8fad6ee40965e72a11ef913d9b96a14432e9d22b
DipeshMahatoTharu <157498235+DipeshMahatoTharu@users.noreply.github.com>
2026-06-29 12:38:03 +0545

    Updated dat 13 par1 completed

--- python basic/Day13/main.py
+++ python basic/Day13/main.py
@@ -21,11 +21,52 @@
 # print(student1.age) 
 
 
-class Dog:
+# class Dog:
+
+#     pass
+
+# dog1 = Dog()
+# dog2 = Dog()
+
+# print(dog1)
+# print(dog2)
+
+
+
+# Exercise 1
+
+# Create a class Car, create 2 objects, and print them.
+class Car:
     pass
 
-dog1 = Dog()
-dog2 = Dog()
 
-print(dog1)
-print(dog2)
+car1=Car()
+car2 =Car()
+
+print(car1)
+print(car2)
+
+
+
+# Exercise 2
+
+# Create a class Phone, create 3 objects, and print them.
+class Phone:
+    pass
+
+phone1=Phone()
+phone2=Phone()
+phone3=Phone()
+
+
+
+print(phone1)
+print(phone2)
+print(phone3)
+# Exercise 3
+
+# Create your own class Laptop with 2 objects and print them.
+
+# Exercise 4
+
+# Create a class Book with 3 objects and print them.
